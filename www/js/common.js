ec2d22f6fca257c251dd60b3bea497a9b32ef4fc
Watt47 <42667102+Watt47@users.noreply.github.com>
2020-02-18 17:12:21 +0300

    Update common.js

--- www/js/common.js
+++ www/js/common.js
@@ -1,6 +1,6 @@
-var common = {
+var common = {//these are common functions to be shared among other web apps
     init: function(){
-        Element.prototype.createChildElement = function(tag, attribute){
+        Element.prototype.createChildElement = function(tag, attribute){//creates child with given attributes and returns it
             var Child = document.createElement(tag);
             if (attribute)
                 attribute.split(',').forEach(function(item){
@@ -9,10 +9,10 @@ var common = {
             this.appendChild(Child);
             return Child;
         };
-        Element.prototype.setResId = function(identifier){
+        Element.prototype.setResId = function(identifier){//set localization identifier (html10n.js)
             this.setAttribute("data-l10n-id", identifier);
         };
-        Array.prototype.swapDelete = function(index){
+        Array.prototype.swapDelete = function(index){//speed-up deleting, internet says it is faster than splice
             this[index] = this[this.length - 1];
             this.pop();
         }
